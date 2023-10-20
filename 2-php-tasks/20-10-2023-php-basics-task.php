33734e2351578553488e93276f19993c1784d48a
omarehab <omarehabsal@gmail.com>
2023-10-20 19:56:56 +0300

    solve task php basics

---
+++ 2-php-tasks/20-10-2023-php-basics-task.php
@@ -0,0 +1,184 @@
+<?php
+// // 1- Check The Number Is Even or Odd
+// $num = 6;
+// if ($num % 2 == 0) {
+//     echo "num is even";
+// } else {
+//     echo "num is odd";
+// }
+
+
+
+
+// 2- Fizz Buzz Game      
+// $num = 60;
+// if ($num % 3 === 0 and $num % 5 === 0) echo ("FizzBuzz");
+// if ($num % 3 === 0 and !($num % 5 === 0)) echo ("Fizz");
+// if (!($num % 3 === 0) and $num % 5 === 0) echo ("Buzz");
+
+// 3- Reverse String for example welcome => emoclew
+// $str = 'not reversed string';
+// echo strrev($str);
+
+
+
+
+// 4- Compute Circle Area and Circumference
+// $rad = 15;
+// $area = pi() * pow($rad,2);
+// $circumference = 2 * pi()* $rad;
+// echo $area;
+// echo "<br/>";
+// echo $circumference;
+
+
+
+
+// 5- check two given numbers and return true if one of the number is
+// 50 or if their sum is 50.
+// $num1 = 50;
+// $num2 = 0;
+// if ($num1 == 50 or $num2 == 50 or $num1 + $num2 == 50) {
+//     echo "true";
+// }
+
+
+
+
+// 6- check from two given integers, whether one is positive and
+// another one is negative.
+// $num1 = 5;
+// $num2 = -9;
+// if ($num1 > 0) {
+//     echo 'num1 is positive'. "<br />";
+// } else {
+//     echo 'num1 is negative';
+// }
+
+// if ($num2 > 0) {
+//     echo 'num2 is positive';
+// } else {
+//     echo 'num2 is negative';
+// }
+
+
+
+
+// 7- check whether a given positive number is a multiple of 5 or a
+// multiple of 8.
+// $num = 80;
+// if ($num % 5 == 0 and $num % 8 == 0) {
+//     echo 'num is a multiple to 5 and also a multiple to 8';
+// } else if ($num % 5 == 0) {
+//     echo 'num is a multiple to 5 ';
+// } else if ($num % 8 == 0) {
+//     echo 'num is a multiple to 8';
+// }
+
+
+
+
+// 8- find the largest of three given integers
+// $num1 = 2;
+// $num2 = 9;
+// $num3 = 19;
+
+// if ($num2 > $num1) {
+//     if ($num2 > $num3) {
+//         echo "num2 is the largest";
+//     } else {
+//         echo "num3 is the largest";
+//     }
+// } else if ($num3 > $num1) {
+//     if ($num3 > $num2) {
+//         echo "num3 is the largest";
+//     } else {
+//         echo "num2 is the largest";
+//     }
+// } else {
+//     echo "num1 is the largest";
+// }
+
+
+
+
+// 9- Compute The sum of the numbers from 1 to 10
+// $sum = 0;
+// for ($x = 1; $x <= 10; $x++) {
+//     $sum += $x;
+// }
+// echo $sum;
+
+
+
+
+// 10- display the pattern like right angle triangle using an asterisk.
+// *
+// **
+// ***
+// ****
+// *****
+// for ($x = 0; $x <= 5; $x++) {
+//     for ($i = 0; $i <= $x; $i++) {
+//         echo "*";
+//     }
+//     echo "<br />";
+// }
+
+
+
+
+
+// 11- check whether a given number is positive or negative.
+// $num = 1;
+// if ($num == 0) {
+//     echo "num equals zero";
+// } else if ($num > 0) {
+//     echo "num is positive";
+// } else {
+//     echo "num is positive";
+// }
+
+
+
+
+// 12- store elements in an array and print it.
+// Test Data:
+// Input 10 elements in the array:
+// element - 0 : 1
+// element - 1 : 1
+// element - 2 : 2
+// $array = [];
+// array_push($array, 0);
+// array_push($array, 1);
+// array_push($array, 2);
+// for ($i = 0; $i < count($array) ; $i++) {
+//     echo " Element - $i is {$array[$i]} <br>";
+// }
+
+
+
+
+// 13- print the sum of two numbers
+// $num1 = 1;
+// $num2 = 20;
+// echo ($num1 + $num2) ;
+
+
+
+// 14- calculate the factorial of a given number
+// Test Data :Input the number : 5
+// Expected Output :
+// The Factorial of 5 is: 120
+// $num = 5;
+// $sum = 1;
+// for ($i = 1 ; $i <= $num; $i++) {
+//     $sum*=$i;
+// }
+// echo $sum ;
+
+
+
+
+// 15- Create Simple Calculator (+ , - , * , /)
+
