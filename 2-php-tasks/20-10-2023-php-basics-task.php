39839283e2432fd56899e92a441edf43aca5c5d2
omarehab <omarehabsal@gmail.com>
2023-10-27 15:26:36 +0300

    turn quiz to functions

--- 2-php-tasks/20-10-2023-php-basics-task.php
+++ 2-php-tasks/20-10-2023-php-basics-task.php
@@ -1,113 +1,147 @@
 <?php
 // // 1- Check The Number Is Even or Odd
-// $num = 6;
-// if ($num % 2 == 0) {
-//     echo "num is even";
-// } else {
-//     echo "num is odd";
+
+// function isEvenOrOdd($num)
+// {
+//     if ($num % 2 == 0) {
+//         echo "num is even";
+//     } else {
+//         echo "num is odd";
+//     }
 // }
 
+// isEvenOrOdd(1);
 
 
 
-// 2- Fizz Buzz Game      
-// $num = 60;
-// if ($num % 3 === 0 and $num % 5 === 0) echo ("FizzBuzz");
-// if ($num % 3 === 0 and !($num % 5 === 0)) echo ("Fizz");
-// if (!($num % 3 === 0) and $num % 5 === 0) echo ("Buzz");
+
+// 2- Fizz Buzz Game    
+
+// function fizzBuzz($num)
+// {
+//     if ($num % 3 === 0 and $num % 5 === 0) echo ("FizzBuzz");
+//     if ($num % 3 === 0 and !($num % 5 === 0)) echo ("Fizz");
+//     if (!($num % 3 === 0) and $num % 5 === 0) echo ("Buzz");
+// }
+// fizzBuzz(45);
+
+
 
 // 3- Reverse String for example welcome => emoclew
-// $str = 'not reversed string';
-// echo strrev($str);
+
+// function revString($str)
+// {
+
+//     echo strrev($str);
+// }
+// revString("omar ehab");
 
 
 
 
 // 4- Compute Circle Area and Circumference
-// $rad = 15;
-// $area = pi() * pow($rad,2);
-// $circumference = 2 * pi()* $rad;
-// echo $area;
-// echo "<br/>";
-// echo $circumference;
 
+// function comAreaAndCircumference($rad){
+//     $area = pi() * pow($rad,2);
+//     $circumference = 2 * pi()* $rad;
+//     echo $area;
+//     echo "<br/>";
+//     echo $circumference;
+// }
+// comAreaAndCircumference(9);
 
 
 
 // 5- check two given numbers and return true if one of the number is
 // 50 or if their sum is 50.
-// $num1 = 50;
-// $num2 = 0;
-// if ($num1 == 50 or $num2 == 50 or $num1 + $num2 == 50) {
-//     echo "true";
+
+// function checkNumber($num1, $num2){
+//     if ($num1 == 50 or $num2 == 50 or $num1 + $num2 == 50) {
+//       return true;
+//     }
+//     return false;
 // }
+// echo checkNumber(1, 50);
 
 
 
 
 // 6- check from two given integers, whether one is positive and
 // another one is negative.
-// $num1 = 5;
-// $num2 = -9;
-// if ($num1 > 0) {
-//     echo 'num1 is positive'. "<br />";
-// } else {
-//     echo 'num1 is negative';
-// }
 
-// if ($num2 > 0) {
-//     echo 'num2 is positive';
-// } else {
-//     echo 'num2 is negative';
+// function checkPositiveOrNegative($num1, $num2)
+// {
+
+//     if ($num1 > 0) {
+//         echo 'num1 is positive' . "<br />";
+//     } else {
+//         echo 'num1 is negative';
+//     }
+
+//     if ($num2 > 0) {
+//         echo 'num2 is positive';
+//     } else {
+//         echo 'num2 is negative';
+//     }
 // }
+// checkPositiveOrNegative(1, 2);
 
 
 
 
 // 7- check whether a given positive number is a multiple of 5 or a
 // multiple of 8.
-// $num = 80;
-// if ($num % 5 == 0 and $num % 8 == 0) {
-//     echo 'num is a multiple to 5 and also a multiple to 8';
-// } else if ($num % 5 == 0) {
-//     echo 'num is a multiple to 5 ';
-// } else if ($num % 8 == 0) {
-//     echo 'num is a multiple to 8';
+// function checkNumIsMulOf80($num)
+// {
+
+//     if ($num % 5 == 0 and $num % 8 == 0) {
+//         echo 'num is a multiple to 5 and also a multiple to 8';
+//     } else if ($num % 5 == 0) {
+//         echo 'num is a multiple to 5 ';
+//     } else if ($num % 8 == 0) {
+//         echo 'num is a multiple to 8';
+//     }
 // }
+// checkNumIsMulOf80(160);
 
 
 
 
 // 8- find the largest of three given integers
-// $num1 = 2;
-// $num2 = 9;
-// $num3 = 19;
+// function findTheLargest($num1,    $num2,    $num3)
+// {
 
-// if ($num2 > $num1) {
-//     if ($num2 > $num3) {
-//         echo "num2 is the largest";
+//     if ($num2 > $num1) {
+//         if ($num2 > $num3) {
+//             echo "num2 is the largest";
+//         } else {
+//             echo "num3 is the largest";
+//         }
+//     } else if ($num3 > $num1) {
+//         if ($num3 > $num2) {
+//             echo "num3 is the largest";
+//         } else {
+//             echo "num2 is the largest";
+//         }
 //     } else {
-//         echo "num3 is the largest";
+//         echo "num1 is the largest";
 //     }
-// } else if ($num3 > $num1) {
-//     if ($num3 > $num2) {
-//         echo "num3 is the largest";
-//     } else {
-//         echo "num2 is the largest";
-//     }
-// } else {
-//     echo "num1 is the largest";
 // }
-
+// findTheLargest(30, 6, 45);
 
 
 
 // 9- Compute The sum of the numbers from 1 to 10
-// $sum = 0;
-// for ($x = 1; $x <= 10; $x++) {
-//     $sum += $x;
+// function sumFrom1To10()
+// {
+
+//     $sum = 0;
+//     for ($x = 1; $x <= 10; $x++) {
+//         $sum += $x;
+//     }
+//     echo $sum;
 // }
-// echo $sum;
+// sumFrom1To10();
 
 
 
@@ -118,28 +152,35 @@
 // ***
 // ****
 // *****
-// for ($x = 0; $x <= 5; $x++) {
-//     for ($i = 0; $i <= $x; $i++) {
-//         echo "*";
-//     }
-//     echo "<br />";
-// }
 
+// function printStars()
+// {
+
+//     for ($x = 0; $x <= 5; $x++) {
+//         for ($i = 0; $i <= $x; $i++) {
+//             echo "*";
+//         }
+//         echo "<br />";
+//     }
+// }
+// printStars();
 
 
 
 
 // 11- check whether a given number is positive or negative.
-// $num = 1;
-// if ($num == 0) {
-//     echo "num equals zero";
-// } else if ($num > 0) {
-//     echo "num is positive";
-// } else {
-//     echo "num is positive";
+// function checkSign($num){
+
+//     if ($num == 0) {
+//         echo "num equals zero";
+//     } else if ($num > 0) {
+//         echo "num is positive";
+//     } else {
+//         echo "num is positive";
+//     }
+
 // }
-
-
+// checkSign(5);
 
 
 // 12- store elements in an array and print it.
@@ -160,9 +201,12 @@
 
 
 // 13- print the sum of two numbers
-// $num1 = 1;
-// $num2 = 20;
-// echo ($num1 + $num2) ;
+// function printSum($num1, $num2)
+// {
+//     echo ($num1 + $num2);
+// }
+// printSum(263, 9);
+
 
 
 
@@ -170,15 +214,41 @@
 // Test Data :Input the number : 5
 // Expected Output :
 // The Factorial of 5 is: 120
-// $num = 5;
-// $sum = 1;
-// for ($i = 1 ; $i <= $num; $i++) {
-//     $sum*=$i;
-// }
-// echo $sum ;
+// function factorial($num)
+// {
+//     // $sum = 1;
+//     // for ($i = 1; $i <= $num; $i++) {
+//     //     $sum *= $i;
+//     // }
+//     // echo $sum;
 
+//     if ($num == 1) {
+//         return 1;
+//     }
+//     return $num *  factorial($num - 1);
+// }
+// echo factorial(5);
 
 
 
 // 15- Create Simple Calculator (+ , - , * , /)
 
+function calc($num1, $num2, $op)
+{
+    switch ($op) {
+        case '+':
+            return $num1 + $num2;
+
+        case '-':
+            return $num1 - $num2;
+
+        case '*':
+            return $num1 * $num2;
+
+        case '/':
+            return $num1 / $num2;
+        default:
+            return 'bad operation';
+    }
+}
+echo calc(65, 8, '+');
